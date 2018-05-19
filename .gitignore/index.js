e97866c90b3bae7fdde697edfb247ebdb2219f78
TiPlayer0 <36779146+TiPlayer0@users.noreply.github.com>
2018-05-19 17:52:02 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,32 +1,48 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-var prefix = ("*")
+var prefix = ("!")
 
 bot.on('ready', function() {
-    bot.user.setActivity("Work-in progres");
-    console.log("Connectedç");
+    bot.user.setGame("Command: !help");
+    console.log("Connected");
 });
 
-bot.login("NDE2NzExOTA0MzI5ODU5MDcy.DXTVCQ.dMo7DbMxfD2grXGqTl8IssIxXow");
+bot.login(process.env.TOKEN);
+
 
 bot.on('message', message => {
     if (message.content === prefix + "help"){
-        message.channel.send("Liste des commandes: \n -*help");
-    }
-    
-bot.on('message', message => {
-    if (message.content === prefix + "tt"){
-        message.channel.send("Le Bot a était crée le 26/02/2018 à 01:04");
-    }
-    
-bot.on('message', message => {
-    if (message.content === prefix + "fabriquand"){
-        message.channel.send("Le Bot a était crée par TiPlayer_0");
+        message.channel.sendMessage("```Liste des commandes: \n !pandabot \n !fabriquant \n !pandamusic```");
     }
 
-    if (message.content === "Salut"){
-        message.reply("Bien le bonjour. =D");
-        console.log("Commande Salut effectué");
+    if (message.content === prefix + "pandabot"){
+        message.channel.sendMessage("```Création du bot le _19/05/2018_ à _17:50_```");
+        console.log("Commande effectué");
     }
-},);
+
+    if (message.content === prefix + "fabriquant"){
+        message.channel.sendMessage("```Ce bot a été crée par TiPlayer```");
+        console.log("Commande effectué");
+    }
+    
+    if (message.content === prefix + "pandamusic play"){
+        message.channel.sendMessage("```pandaMusic bientôt disponible !```");
+        console.log("Commande effectué");
+    }
+    
+    if (message.content === prefix + "pandamusic stop"){
+        message.channel.sendMessage("```pandaMusic bientôt disponible !```");
+        console.log("Commande effectué");
+    }
+    
+    if (message.content === prefix + "pandamusic next"){
+        message.channel.sendMessage("```pandaMusic bientôt disponible !```");
+        console.log("Commande effectué");
+    }
+    
+    if (message.content === prefix + "pandamusic"){
+        message.channel.sendMessage("```pandaMusic:\n !pandamusic play\n !pandamusic next\n !pandamusic stop```");
+        console.log("Commande effectué");
+    }
+});
